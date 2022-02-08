9b281cb0e301333a206ad177f7a22524b09dcddb
wanshouxujiu <97508633+wanshouxujiu@users.noreply.github.com>
2022-02-08 16:11:00 +0800

    Update 杂1.c

--- 杂1.c
+++ 杂1.c
@@ -46,3 +46,74 @@ int main()
 
 	return 0;
 }
+
+//===================================模拟函数strcpy====================================
+
+//void my_strcpy(char* dest, char* src)
+//{
+//	while (*src != 0)
+//	{
+//		*dest = *src;
+//		dest++;
+//		src++;
+//	}
+//	*dest = *src;
+//}
+
+//优化
+
+//void my_strcpy(char* dest, char* src)
+//{
+//	if (dest != NULL && src != NULL) //空指针不能用，传过来会让程序崩溃
+//	{
+//		while (*dest++ = *src++)//为了把\0也拷贝
+//		{
+//			;
+//		}
+//	}
+//}
+
+//断言
+
+//void my_strcpy(char* dest, char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//}
+
+//int main()
+//{
+//	char arr1[] = "################";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//
+//	return 0;
+//}
+
+//满分版
+
+char* my_strcpy(char* dest, const char* src) //const
+{
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return ret;
+}
+
+int main()
+{
+	char arr1[] = "################";
+	char arr2[] = "bit";
+	printf("%s", my_strcpy(arr1, arr2));
+
+	return 0;
+}
