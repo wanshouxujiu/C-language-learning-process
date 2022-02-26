082748e537f1d581e2a7fb994c71fc9e7482f873
wanshouxujiu <97508633+wanshouxujiu@users.noreply.github.com>
2022-02-26 18:49:48 +0800

    Add files via upload

---
+++ C语言进阶篇/指针_长度.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+int main()
+{
+	//int a[] = { 1,2,3,4 };
+	//printf("%d\n",sizeof(a));
+	//printf("%d\n",sizeof(a + 0));
+	//printf("%d\n",sizeof(*a));
+	//printf("%d\n",sizeof(a + 1));
+	//printf("%d\n",sizeof(a[1]));
+	//printf("%d\n",sizeof(&a));
+	//printf("%d\n",sizeof(*&a));
+	//printf("%d\n",sizeof(&a + 1));
+	//printf("%d\n",sizeof(&a[0]));
+	//printf("%d\n", sizeof(&a[0] + 1));
+
+
+	//char arr[] = { 'a', 'b', 'C', 'd', 'e', 'f' };
+	//printf("%d\n",sizeof(arr));
+	//printf("%d\n",sizeof(arr + 0));
+	//printf("%d\n",sizeof(*arr));
+	//printf("%d\n",sizeof(arr[1]));
+	//printf("%d\n",sizeof(&arr));
+	//printf("%d\n",sizeof(&arr + 1));
+	//printf("%d\n",sizeof(&arr[0] + 1));
+
+	//printf("%d\n", strlen(arr));
+	//printf("%d\n", strlen(arr + 0));
+	//printf("%d\n", strlen(*arr));	//strlen会把输入的值作为地址,非法访问内存
+	//printf("%d\n", strlen(arr[1]));
+	//printf("%d\n", strlen(&arr));
+	//printf("%d\n", strlen(&arr + 1));
+	//printf("%d\n", strlen(&arr[0] + 1));
+
+
+	//char arr[] = "abcdef";
+	//printf("%d\n", sizeof(arr));
+	//printf("%d\n", sizeof(arr + 0));
+	//printf("%d\n", sizeof(*arr));
+	//printf("%d\n", sizeof(arr[1]));
+	//printf("%d\n", sizeof(&arr));
+	//printf("%d\n", sizeof(&arr + 1));
+	//printf("%d\n", sizeof(&arr[0] + 1));
+
+	//printf("%d\n", strlen(arr));
+	//printf("%d\n", strlen(arr + 0));
+	//printf("%d\n", strlen(*arr));
+	//printf("%d\n", strlen(arr[1]));
+	//printf("%d\n", strlen(&arr));
+	//printf("%d\n", strlen(&arr + 1));
+	//printf("%d\n", strlen(&arr[0] + 1));
+
+
+	//char* p = "abcdef";
+
+	//printf("%d\n", sizeof(p));
+	//printf("%d\n", sizeof(p + 1));
+	//printf("%d\n", sizeof(*p));
+	//printf("%d\n", sizeof(p[0]));
+	//printf("%d\n", sizeof(&p));
+	//printf("%d\n", sizeof(&p + 1));
+	//printf("%d\n", sizeof(&p[0] + 1));
+
+	//printf("%d\n", strlen(p));
+	//printf("%d\n", strlen(p + 1));
+	//printf("%d\n", strlen(*p));
+	//printf("%d\n", strlen(p[0]));
+	//printf("%d\n", strlen(&p));	//指针变量的地址
+	//printf("%d\n", strlen(&p + 1));
+	//printf("%d\n", strlen(&p[0] + 1));
+
+
+	//int a[3][4] = { 0 };
+	//printf("%d\n", sizeof(a));
+	//printf("%d\n", sizeof(a[0][0]));
+	//printf("%d\n", sizeof(a[0]));
+	//printf("%d\n", sizeof(a[0] + 1));
+	//printf("%d\n", sizeof(*(a[0] + 1)));
+	//printf("%d\n", sizeof(a + 1));
+	//printf("%d\n", sizeof(*(a + 1)));
+	//printf("%d\n", sizeof(&a[0] + 1));
+	//printf("%d\n", sizeof(*(&a[0] + 1)));
+	//printf("%d\n", sizeof(*a));
+	//printf("%d\n", sizeof(a[3]));
+
+
+	return 0;
+}
